5d321eba6e03cb7051fa7fc27273617476e481dd
Koushik3107 <koushik.debnath1989@gmail.com>
2022-06-04 12:29:50 +0530

    intial commit

---
+++ src/main/java/com/example/simplilearn/SimplilearnPhase5Application.java
@@ -0,0 +1,13 @@
+package com.example.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplilearnPhase5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnPhase5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplilearn/SimplilearnPhase5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplilearnPhase5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
